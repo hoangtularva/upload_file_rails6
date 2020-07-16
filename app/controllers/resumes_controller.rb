c36e2272f417d35e97b13f63ba1c515f764dad04
hoangtularva <hoangtukg295@gmail.com>
2020-07-16 11:02:28 +0700

    add boostrap

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,13 +1,31 @@
 class ResumesController < ApplicationController
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def destroy
-  end
+  def index   
+    @resumes = Resume.all   
+ end   
+    
+ def new   
+    @resume = Resume.new   
+ end   
+    
+ def create   
+    @resume = Resume.new(resume_params)   
+       
+    if @resume.save   
+       redirect_to resumes_path, notice: "Successfully uploaded."   
+    else   
+       render "new"   
+    end   
+       
+ end   
+    
+ def destroy   
+    @resume = Resume.find(params[:id])   
+    @resume.destroy   
+    redirect_to resumes_path, notice:  "Successfully deleted."   
+ end   
+    
+ private   
+    def resume_params   
+    params.require(:resume).permit(:name, :attachment)   
+ end   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'resumes/index'
-  get 'resumes/new'
-  get 'resumes/create'
-  get 'resumes/destroy'
   resources :resumes, only: [:index, :new, :create, :destroy]   
    root "resumes#index"  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
