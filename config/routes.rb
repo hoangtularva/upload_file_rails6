fef0e123e327d2de7a538e682a3dd341124fd11c
hoangtularva <hoangtukg295@gmail.com>
2020-07-16 11:20:22 +0700

    add new images

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   resources :resumes, only: [:index, :new, :create, :destroy]   
    root "resumes#index"  
+   get 'resumes/new'
+   get 'resumes/create'
+   get 'resumes/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
